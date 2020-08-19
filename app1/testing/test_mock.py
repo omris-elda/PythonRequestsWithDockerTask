07b98686ec86ad264b3aeb44668d0cb23d055a13
omris-elda <gentlestpuppy@hotmail.co.uk>
2020-08-19 12:48:50 +0000

    removed cache files

---
+++ app1/testing/test_mock.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+
+from app import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+# class TestResponse(TestBase):
+
+#     def test_apes(self):
+#         with patch("requests.get") as g:
+#             g.return_value.text = "Apes"
+
+#             response = self.client.get(url_for("generate"))
+#             self.assertIn(b"Gibber", response.data)
